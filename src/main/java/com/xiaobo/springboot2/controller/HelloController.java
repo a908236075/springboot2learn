dc14f7182ff40b5a0fe75837925a2c763bba3c48
lbt <908236075@qq.com>
2021-03-08 19:05:14 +0800

    修改部分代码

--- src/main/java/com/xiaobo/springboot2/controller/HelloController.java
+++ src/main/java/com/xiaobo/springboot2/controller/HelloController.java
@@ -1,16 +1,63 @@
 package com.xiaobo.springboot2.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
 public class HelloController {
 
 //    @GetMapping("/hello")
     @RequestMapping("/hello")
     public String sayHello(){
         return "Hello!";
+    }
+  /*  @RequestMapping("/index")
+    public String index(){
+        return "index.html";
+    }*/
+
+    @GetMapping("/params")
+    public String testParam(Map<String,Object> map,
+                            Model model,
+                            HttpServletRequest request,
+                            HttpServletResponse response){
+        map.put("hello","world666");
+        model.addAttribute("world","hello666");
+        request.setAttribute("message","HelloWorld");
+
+        Cookie cookie = new Cookie("c1","v1");
+        response.addCookie(cookie);
+        return "forward:/success";
+    }
+
+
+    @ResponseBody
+    @GetMapping("/success")
+    public Map success(@RequestAttribute(value = "msg",required = false) String msg,
+                       @RequestAttribute(value = "code",required = false)Integer code,
+                       HttpServletRequest request){
+        Object msg1 = request.getAttribute("msg");
+
+        Map<String,Object> map = new HashMap<>();
+        Object hello = request.getAttribute("hello");
+        Object world = request.getAttribute("world");
+        Object message = request.getAttribute("message");
+
+        map.put("reqMethod_msg",msg1);
+        map.put("annotation_msg",msg);
+        map.put("hello",hello);
+        map.put("world",world);
+        map.put("message",message);
+
+        return map;
+
     }
 
 }
